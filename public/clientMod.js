a11c99807da75b7815f53853c27402f44083acea
Zeprit <kyonedelenbosch@live.nl>
2021-01-21 15:55:59 +0100

    Update clientMod.js

--- public/clientMod.js
+++ public/clientMod.js
@@ -435,9 +435,9 @@ function firstFloorEnter(playerId, roomId) {
       setVideo("BmdHXUlAIgc");
 			//setVideo("playlist&list=PLNiztf0SpnykWe8WJuka-EPJLvKCNg5XC");
 		}
-		e = document.getElementById("talk-form");
-		if (e != null)
-			e.style.display = "none";
+	  //e = document.getElementById("talk-form");
+		//if (e != null)
+			//e.style.display = "none";
 
 		//e = document.getElementById("no-talkie");
 		//if (e != null)
@@ -459,9 +459,9 @@ function firstFloorEnter(playerId, roomId) {
 			player = select("#yt_video");
 			player.attribute('src', videoUrl);
 		}
-		e = document.getElementById("video-container");
-		if (e != null)
-			e.style.display = "block";
+	//	e = document.getElementById("video-container");
+		//if (e != null)
+		//	e.style.display = "block";
 	}
 
 	//roomnameExit: called right before a player exits or disconnects
