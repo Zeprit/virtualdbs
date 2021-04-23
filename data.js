1bde646f36b0e328549b5e779828d54df89a441c
Carcassii <48177997+Carcassii@users.noreply.github.com>
2021-04-23 22:05:29 +0200

    Update data.js

--- data.js
+++ data.js
@@ -68,7 +68,7 @@ module.exports.ROOMS =
         areaColors: {
             //h will be replaced by #
             hff0000: { cmd: "enter", room: "dBsBeerGarten", label: "Beer Garten", point: [50,35], enterPoint: [128,189], obstacle: false },
-            h00ffbc: { cmd: "enter", room: "dBsBar", label: "Bar", point: [156, 184], enterPoint: [156, 26], obstacle: false },
+            h00ffbc: { cmd: "enter", room: "dBsBar", label: "Bar", point: [156, 184], enterPoint: [156, 30], obstacle: false },
         },
         things: {
 
